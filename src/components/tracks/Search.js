58f4828667de358fb4b8e9912fab620eea4c3ea7
hamid842 <h_mohammadi842@yahoo.com>
2020-05-16 19:48:34 +0430

    Final commit

---
+++ src/components/tracks/Search.js
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import axios from "axios";
+import { Consumer } from "../../context";
+
+class Search extends Component {
+  state = {
+    tarckTitle: "",
+  };
+
+  onChange = (e) => {
+    this.setState({ tarckTitle: e.target.value });
+  };
+
+  findTrack = (dispatch, e) => {
+    e.preventDefault();
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?q_track=${this.state.tarckTitle}&page_size=10&page=1&s_track_rating=desc&apikey=${process.env.REACT_APP_MM_KEY}`
+      )
+      .then((res) => {
+        dispatch({
+          type: "SEARCH_TRACKS",
+          payload: res.data.message.body.track_list,
+        });
+        this.setState({ tarckTitle: "" });
+      })
+      .catch((err) => console.log(err));
+  };
+  render() {
+    return (
+      <Consumer>
+        {(value) => {
+          const { dispatch } = value;
+          return (
+            <div className="card card-body mb-4 p-4">
+              <h1 className="display-4 text-center">
+                <i className="fa fa-music">Search For a Song</i>
+              </h1>
+              <p className="lead text-center">Get the lyrics for any song</p>
+              <form onSubmit={this.findTrack.bind(this, dispatch)}>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="song title ..."
+                    name="trackTitle"
+                    value={this.state.tarckTitle}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <button
+                  className="btn btn-primary btn-block mb-5"
+                  type="submit"
+                >
+                  Get Track Lyrics
+                </button>
+              </form>
+            </div>
+          );
+        }}
+      </Consumer>
+    );
+  }
+}
+export default Search;
